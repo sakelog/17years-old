5f872e8f92743b6656e3a1e1f1e96095b533beec
sake <sakelog.website@gmail.com>
2021-01-04 07:26:44 +0900

    画像表示テスト

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   images: {
     loader: 'cloudinary',
-    path: 'https://res.cloudinary.com/cl1sakelog/17years_old',
+    path: 'https://res.cloudinary.com/cl1sakelog/',
   },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
