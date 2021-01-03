b39ec16d7496f328240f01112fc11f2ce95ce70c
sakelog <sakelog@users.noreply.github.com>
2021-01-03 22:03:54 +0000

    deploy: 76536c1ac9804fa17c54f636c1a12fdeae276a54

---
+++ _next/static/8msUCLw_DW4ENe63rFOLE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F938d318fc5398da5f077.css","static\u002Fchunks\u002Fpages\u002Findex-abf0eff4cfbc6bfd6091.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-85785763783c1e33a9f3.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
