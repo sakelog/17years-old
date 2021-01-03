f6ddadd661e83947fa94149733c69618fc27db26
sake <sakelog.website@gmail.com>
2021-01-04 07:09:33 +0900

    公開に向けた設定

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const path = require('path');
+
 module.exports = {
   images: {
     loader: 'cloudinary',
     path: 'https://res.cloudinary.com/cl1sakelog/17years_old',
   },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  basePath: '/17years-old',
 };
